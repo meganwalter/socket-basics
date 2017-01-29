6007be34f92e2133b31bbcaf1b135b6205b0375c
Megan Walter <megwalter@hotmail.com>
2017-01-29 17:55:23 -0500

    added front end text box and ability to send messages

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,18 @@ socket.on('message', function (message) {
   console.log('new message:');
   console.log(message.text);
 });
+
+//Handles submitting of new message
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+  event.preventDefault();
+
+  socket.emit('message', {
+    text: $form.find('input[name=message]').val()
+  });
+
+  $form.find('input[name=message]').val('');
+  $('input[name=message]').focus();
+});
