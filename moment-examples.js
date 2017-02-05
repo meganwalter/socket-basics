6e6c5e242c3aa5201691333264f6964088fee9a3
Megan Walter <megwalter@hotmail.com>
2017-02-05 10:12:16 -0500

    adding moment to do timestamps

---
+++ moment-examples.js
@@ -0,0 +1,22 @@
+var moment = require('moment');
+var now = moment();
+
+// console.log(now.format());
+// console.log(now.format('X'));
+// console.log(now.format('x'));
+// console.log(now.valueOf());
+
+var timestamp = 1486307014814;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('hh:mm a')); //11:06 am
+
+
+
+// now.subtract(1, 'year');
+//
+// console.log(now.format());
+//
+// console.log(now.format('MMM Do YYYY, h:mma')); // 6:45 pm
+//
+// //add month and day, Oct 5th 2017, 6:45pm
