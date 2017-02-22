96b573f7ce30fdb5d040ec900cb9b7aa7f3549bb
Megan Walter <megwalter@hotmail.com>
2017-02-21 21:09:26 -0500

    added name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,6 @@
+var person = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+console.log(person + " joined " + room + "!");
 var socket = io();
 
 socket.on('connect', function () {
@@ -6,10 +9,12 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
   var momentTimeStamp = moment.utc(message.timestamp);
+  var $message = jQuery('.messages');
   console.log('new message:');
   console.log(message.text);
 
-  jQuery('.messages').append('<p>' + '<strong>' + momentTimeStamp.local().format('hh:mm a') + '</strong>' + ': ' + message.text + '</p>');
+  $message.append('<p><strong>' + message.name + ' ' + momentTimeStamp.local().format('hh:mm a') + '</strong></p>');
+  $message.append('<p>' + message.text + '</p>');
 });
 //also concat formatted time stamp
 
@@ -20,10 +25,13 @@ var $form = jQuery('#message-form');
 $form.on('submit', function (event) {
   event.preventDefault();
 
+  var $message = $form.find('input[name=message]');
+
   socket.emit('message', {
-    text: $form.find('input[name=message]').val()
+    name: person,
+    text: $message.val()
   });
 
-  $form.find('input[name=message]').val('');
-  $('input[name=message]').focus();
+  $message.val('');
+
 });
